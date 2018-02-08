b900ea9ce994a04af758923874011e51f3fb588e
seeker <yanlei0419@163.com>
2018-02-08 16:26:49 +0800

    删除文件路径

--- 创建oracle文件路径.sql
+++
@@ -1,3 +0,0 @@
-create or replace directory PMTOEDWDATA as '/pmb';   
-grant read,write on directory PMTOEDWDATA to prdpmb;   
-GRANT EXECUTE ON utl_file TO prdpmb;  
--- 数据库表被锁.sql
+++
@@ -1,7 +0,0 @@
-SELECT object_name, machine, s.sid, s.serial#
-  FROM gv$locked_object l, dba_objects o, gv$session s
- WHERE l.object_id¡¡ = o.object_id
-   AND l.session_id = s.sid;
-
---ALTER system kill session '293, 9498'; 
---ALTER system kill session '294, 21948'; 
